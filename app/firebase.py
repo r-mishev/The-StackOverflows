c6bda6a3afac800ccebe8e88c47d87f3a7923402
Teodor Kovachev <tedox.k@gmail.com>
2025-03-30 05:52:54 +0300

    fix: firebase issues

--- app/firebase.py
+++ app/firebase.py
@@ -1,11 +1,11 @@
 import os
+import json
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-SERVICE_ACCOUNT_PATH = os.path.join(BASE_DIR, 'serviceAccountKey.json')
+json_str = os.getenv("SERVICE_ACCOUNT_JSON")
+cred_dict = json.loads(json_str)
+cred = credentials.Certificate(cred_dict)
 
-cred = credentials.Certificate(SERVICE_ACCOUNT_PATH)
 firebase_admin.initialize_app(cred)
-
 db = firestore.client()
